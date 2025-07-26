43a504e8629bd516cf3d3477a039b6fc1fb5caad
abeerfedo <adamabeer44@gmail.com>
2025-07-26 09:48:05 +0300

    add images and basic code

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-function Book(title , author , pages , read){
+let myLibrary =[];
+
+function Book(title , author , pages , read, id){
     if(!new.target){
         throw Error("error baby")
     }
@@ -6,12 +8,23 @@ function Book(title , author , pages , read){
     this.author=author;
     this.pages= pages;
     this.read=read;
+    this.id= id;
 
-    this.info= function(){
-        let stat= read ?  "yes red":"not read yet" ;
-        return `${title}. by ${author}, ${pages} pages, ${stat}`;
-    }
+    // this.info= function(){
+    //     let stat= read ?  "yes red":"not read yet" ;
+    //     return `${title}. by ${author}, ${pages} pages, ${stat}`;
+    // }
 }
 
-const bookone= new Book("some", "me", 3232, false)
-console.log(bookone.info())
+function addBookToLibrary(title, author, pages , read) {
+
+    let id= crypto.randomUUID();
+    const newBook= new Book(title, author, pages , read , id);
+    myLibrary.push(newBook);
+
+}
+
+// const bookone= new Book("some", "me", 3232, false)
+// console.log(bookone.info())
+addBookToLibrary("some", "me", 3232, false)
+console.log(myLibrary)
