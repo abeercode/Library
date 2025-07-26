9f7019f0f604eeef515a713c1c2c94f6555620f0
abeerfedo <adamabeer44@gmail.com>
2025-07-26 12:01:21 +0300

    desigh the UI the display function

--- code.js
+++ code.js
@@ -1,30 +1,54 @@
-let myLibrary =[];
+let myLibrary = [];
+const books = document.querySelector(".books");
+const colors=["red.png", "yellow.png", "darkblue.png", "pink.png","green.png","lightblue.png"]
 
-function Book(title , author , pages , read, id){
-    if(!new.target){
+
+function Book(title, author, pages, read, id) {
+    if (!new.target) {
         throw Error("error baby")
     }
-    this.title=title;
-    this.author=author;
-    this.pages= pages;
-    this.read=read;
-    this.id= id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
 
-    // this.info= function(){
-    //     let stat= read ?  "yes red":"not read yet" ;
-    //     return `${title}. by ${author}, ${pages} pages, ${stat}`;
-    // }
+    this.info = function () {
+        let stat = read ? "yes red" : "not read yet";
+        return `${title}. by ${author}, ${pages} pages, ${stat}`;
+    }
 }
 
-function addBookToLibrary(title, author, pages , read) {
+function addBookToLibrary(title, author, pages, read) {
 
-    let id= crypto.randomUUID();
-    const newBook= new Book(title, author, pages , read , id);
+    let id = crypto.randomUUID();
+    const newBook = new Book(title, author, pages, read, id);
     myLibrary.push(newBook);
 
 }
 
-// const bookone= new Book("some", "me", 3232, false)
-// console.log(bookone.info())
-addBookToLibrary("some", "me", 3232, false)
+function displayBooks() {
+    let z= 1;
+    myLibrary.forEach(book => {
+        let abook = document.createElement("div")
+        abook.textContent = book.info();
+        abook.classList.add("abook")
+        let randomBg = colors[Math.floor(Math.random()*colors.length)];
+        let strPath="./images/librarypng/"
+        abook.style.backgroundImage= 'url("'+ strPath + randomBg + '")';
+        abook.style.zIndex=z++;
+        books.insertBefore(abook, books.firstChild)
+        // books.appendChild(abook);
+    });
+
+}
+
+addBookToLibrary("some", "me", 1, false)
+addBookToLibrary("some", "me", 2, false)
+addBookToLibrary("some", "me", 3, false)
+addBookToLibrary("some", "me", 4, false)
+addBookToLibrary("some", "me", 5, false)
+// addBookToLibrary("some", "me", 5, false)
+// addBookToLibrary("some", "me", 5, false)
 console.log(myLibrary)
+displayBooks();
